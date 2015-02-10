28566e9e47fa465e601c6c85ba47763e7c5df9c1
pustserg <psakaskif@gmail.com>
2015-02-10 14:44:29 +0300

    added boostrap

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
+  before_action :set_categories_for_sidebar
   respond_to :html
+
+  private
+  def set_categories_for_sidebar
+    @sidebar_categories = Category.all
+  end
 end
