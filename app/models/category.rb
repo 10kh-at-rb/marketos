944caa882a2bdf65cfde3dfdd7c696bb2733d759
pustserg <psakaskif@gmail.com>
2015-02-10 10:53:01 +0300

    added category model

---
+++ app/models/category.rb
@@ -0,0 +1,15 @@
+# == Schema Information
+#
+# Table name: categories
+#
+#  id          :integer          not null, primary key
+#  title       :string
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+class Category < ActiveRecord::Base
+
+  validates :title, presence: true
+end
---
+++ spec/factories/categories.rb
@@ -0,0 +1,17 @@
+# == Schema Information
+#
+# Table name: categories
+#
+#  id          :integer          not null, primary key
+#  title       :string
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+FactoryGirl.define do
+  factory :category do
+    
+  end
+
+end
