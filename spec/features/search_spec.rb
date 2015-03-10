b590e0df1602a9c9e0d3b1624146267b8b27f45e
pustserg <psakaskif@gmail.com>
2015-03-10 14:02:51 +0300

    search spec does not works

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -4,6 +4,12 @@ feature 'user can find good' do
   given!(:good_with_title) { create(:good, name: 'catch name') }
   given!(:good_with_desc) { create(:good, description: 'catch desc') }
   given!(:wrong_good) { create(:good) }
+  before(:each) do
+    ThinkingSphinx::Test.init
+    ThinkingSphinx::Test.start
+    index
+    # sleep 0.5
+  end
 
   scenario 'user tries to find good by name' do
     visit root_path
@@ -11,6 +17,7 @@ feature 'user can find good' do
       find("input[id='search']").set('catch name')
       find("input[type='submit']").click
     end
+    # save_and_open_page
     expect(page).to have_content 'catch name'
     expect(page).to_not have_content wrong_good.name
   end
@@ -24,4 +31,9 @@ feature 'user can find good' do
     expect(page).to have_content 'catch desc'
     expect(page).to_not have_content wrong_good.description
   end
+
+  after(:all) do
+    ThinkingSphinx::Test.stop
+  end
+  
 end
